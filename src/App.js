a9ff4a4faf8e513e06c927197b29ff581c43ccf8
Justin Brown <justinbrown.work@gmail.com>
2019-03-18 15:47:01 -0500

    Query and display todos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
+import { map } from "async";
+import { compose, graphql } from "react-apollo";
+import gql from "graphql-tag";
+import { listTodos } from "./graphql/queries";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div>
+        {this.props.todos.map(item => (
+          <div>{item.name}</div>
+        ))}
+      </div>
+    );
   }
 }
 
-export default App;
+export default compose(
+  graphql(gql(listTodos), {
+    options: {
+      fetchPolicy: "cache-and-network"
+    },
+    props: props => ({
+      todos: props.data.listTodos ? props.data.listTodos.items : []
+    })
+  })
+)(App);
